e9dff4eb2cfbec05004ba2280a936adfe5c738f3
Trent Mentink <trentmentink@gmail.com>
2017-01-02 19:55:40 -0800

    setup project

--- src/js/base/config.js
+++ src/js/base/config.js
@@ -9,15 +9,7 @@
     // Config Object
     // =======================================
     page.config = {
-      filterIcon: {
-        active: "fa-check-square-o",
-        normal: "fa-square-o"
-      },
-      projectIcon: "fa-ellipsis-v",
-      artworkIcon: {
-        single: "fa-tag",
-        multi: "fa-tags"
-      }
+
     };
 
 
--- src/js/macro/page/page--init.js
+++ src/js/macro/page/page--init.js
@@ -7,52 +7,8 @@
     "use strict";
 
     $cache(document).ready(function() {
-      generalInit();
-
-      if (page.breakpoints.mobile.matches) {
-        page.events.mobile.on();
-      }
-      else if (page.breakpoints.desktop.matches) {
-        page.events.desktop.on();
-      }
-
-      page.breakpoints.mobile.addListener(function(e){
-        if (e.matches) {
-          page.events.mobile.on();
-          page.events.desktop.off();
-        }
-      });
-
-      page.breakpoints.desktop.addListener(function(e){
-        if (e.matches) {
-          page.events.mobile.off();
-          page.events.desktop.on();
-        }
-      });
 
     });
-
-
-    // General Init
-    // ========================================
-    var generalInit = function() {
-      page.loading.init();
-      page.events.general.on();
-    };
-
-
-    // Mobile Init
-    // ========================================
-    var mobileInit = function() {
-      
-    };
-
-
-    // Desktop Init
-    // ========================================
-    var desktopInit = function() {
-
-    };
 
   })(page);
 
